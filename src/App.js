665dba9e56fef8422b81f3c0a49432d79903be0c
ismail <ismailsardar@gmail.com>
2023-02-17 23:32:12 +0600

    admin rout setap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import './App.css';
-import {Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import {Toaster } from 'react-hot-toast'
-import Register from './pages/auth/Register';
-import { BrowserRouter } from 'react-router-dom';
-import Login from './pages/auth/Login';
-import Menu from './components/nav/Menu';
-import PrivateRoute from './components/routes/PrivateRoute';
-import DashBoard from './pages/user/DashBoard';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
+import Register from "./pages/auth/Register";
+import { BrowserRouter } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Menu from "./components/nav/Menu";
+import PrivateRoute from "./components/routes/PrivateRoute";
+import DashBoard from "./pages/user/DashBoard";
+import AdminRoute from "./components/routes/AdminRoute";
+import AdminDashboard from "./pages/admin/Dashboard";
+import AdminCategory from "./pages/admin/Category";
+import AdminProduct from "./pages/admin/Product";
+
+const PageNotFound = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      404 | Page not found
+    </div>
+  );
+};
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Menu />
-      <Toaster />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={<PrivateRoute />}>
-          <Route path='user' element={<DashBoard />} />
-        </Route>
-      </Routes>
+        <Menu />
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<PrivateRoute />}>
+            <Route path="user" element={<DashBoard />} />
+          </Route>
+
+          <Route path="/dashboard" element={<AdminRoute />}>
+            <Route path="admin" element={<AdminDashboard />} />
+            <Route path="admin/category" element={<AdminCategory />} />
+            <Route path="admin/product" element={<AdminProduct />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} replace />
+        </Routes>
       </BrowserRouter>
     </>
   );
