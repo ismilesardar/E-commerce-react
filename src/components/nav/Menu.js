28cf50599e8175d635cf95baed5f5931d6074f2b
ismail <ismailsardar@gmail.com>
2023-02-17 23:26:36 +0600

    admin menu created

--- src/components/nav/Menu.js
+++ src/components/nav/Menu.js
@@ -4,22 +4,22 @@ import { useAuth } from "../../context/auth";
 
 const Menu = () => {
   const [auth, setAuth] = useAuth();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const handelClick = ()=>{
-    setAuth({...auth,user: null,token:""});
+  const handelClick = () => {
+    setAuth({ ...auth, user: null, token: "" });
     localStorage.removeItem("auth");
     navigate("/login");
-  }
+  };
 
   return (
     <>
       <ul
         style={{ height: "50px" }}
-        className="nav d-flex justify-content-center shadow-sm mb-2 bg-dark"
+        className="nav d-flex justify-content-center shadow-sm mb-2 "
       >
         <li className="nav-item">
-          <NavLink to="/" className="nav-link text-light">
+          <NavLink to="/" className="nav-link">
             HOME
           </NavLink>
         </li>
@@ -27,22 +27,45 @@ const Menu = () => {
         {!auth?.user ? (
           <>
             <li className="nav-item">
-              <NavLink to="/login" className="nav-link text-light">
+              <NavLink to="/login" className="nav-link">
                 LOGIN
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/register" className="nav-link text-light">
+              <NavLink to="/register" className="nav-link">
                 REGISTER
               </NavLink>
             </li>
           </>
         ) : (
-          <>
-            <li className="nav-item">
-                <Link onClick={handelClick} className="nav-link text-light">LOGOUT</Link>
+          <div className="dropdown">
+            <li>
+              <Link
+                className="nav-link pointer dropdown-toggle"
+                data-bs-toggle="dropdown"
+              >
+                {auth?.user?.name?.toUpperCase()}
+              </Link>
+
+              <ul className="dropdown-menu">
+                <li>
+                  <NavLink
+                    className="nav-link"
+                    to={`/dashboard/${auth?.user?.role===1 ? "admin" : "user"
+                      }`}
+                  >
+                    Dashboard
+                  </NavLink>
+                </li>
+
+                <li className="nav-item pointer">
+                  <Link onClick={handelClick} className="nav-link text-black">
+                    LOGOUT
+                  </Link>
+                </li>
+              </ul>
             </li>
-          </>
+          </div>
         )}
       </ul>
     </>
