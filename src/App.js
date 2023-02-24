63293ccc73ae066fd13306022ade6c0b3386b40c
ismail <ismailsardar@gmail.com>
2023-02-24 21:42:15 +0600

    make category component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter,Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
 import Register from "./pages/auth/Register";
-import { BrowserRouter } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Menu from "./components/nav/Menu";
 import PrivateRoute from "./components/routes/PrivateRoute";
