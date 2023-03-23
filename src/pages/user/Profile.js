d493fe1cdbb7a8a3c29c0d18f25c4e95522a6fb4
ismail <ismailsardar@gmail.com>
2023-03-23 11:32:19 +0600

    user Profile page add

---
+++ src/pages/user/Profile.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Jumbotron from "../../components/card/Jumbotron";
+import UserMenu from "../../components/nav/UserMenu";
+import { useAuth } from "../../context/auth";
+
+const UsersProfile = () => {
+  const [auth, setAuth] = useAuth();
+
+  return (
+    <>
+      <Jumbotron title={`Hello ${auth?.user?.name}`} subTitle="Dashboard" />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+          <div className="col-md-9">
+            <div className="p-3 mt-2 md-3 h4 bg-light">Profile</div>
+            Update from...
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UsersProfile;
